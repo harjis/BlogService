8ea5679035623fa2d8d936d137dd913d7a592027
Joonas Harjukallio <joonas.harjukallio@relexsolutions.com>
2024-10-09 15:08:20 +0300

    Kafka can be brought up

--- kafka/install.sh
+++ kafka/install.sh
@@ -1,5 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-helm install blog-service-kafka confluentinc/cp-helm-charts -f "${__dir}"/k8s/values.yaml
+kubectl create namespace primary-kafka
+kubectl create namespace cfk
+kubectl config set-context --current --namespace cfk
+
+helm repo add confluentinc https://packages.confluent.io/helm
+helm repo update
+helm upgrade --install confluent-operator \
+  confluentinc/confluent-for-kubernetes \
+  --set namespaceList="{primary-kafka,cfk}" \
+  --namespace cfk \
+  --set namespaced=true
+
+kubectl apply -f "${__dir}"/k8s/values.yaml
+kubectl config set-context --current --namespace default
--- kafka/uninstall.sh
+++ kafka/uninstall.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-helm uninstall blog-service-kafka
+# Not set on purpose. We want to try to uninstall everything even if previous commands fail.
+# This is useful when install succeeds only partially.
+#set -o errexit
+set -o pipefail
+set -o nounset
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-"${DIR}"/clear-kafka-data.sh
+kubectl delete -f "${__dir}"/k8s/values.yaml
+
+helm uninstall confluent-operator
+
+kubectl delete namespace primary-kafka
+kubectl delete namespace cfk
