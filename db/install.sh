0c814d8562e2b1e82db6e22d83bb8d7356b7d897
Joonas Harjukallio <joonas.harjukallio@relexsolutions.com>
2024-10-09 13:27:18 +0300

    Manual db setup

--- db/install.sh
+++ db/install.sh
@@ -3,7 +3,4 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 kubectl apply -f "${__dir}"/k8s/db-pvc.yaml
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm install postgresql bitnami/postgresql -f "${__dir}"/k8s/values.yaml
+kubectl apply -f "${__dir}"/k8s/db.yaml
